7693ff6fada6d868656175525ad154b04eded441
shorot <shhoroth120@gmail.com>
2015-11-27 20:30:38 +0600

    CropCategory added

---
+++ Database/cropCategory.sql
@@ -0,0 +1,4 @@
+insert into CropCategory(ID,Name) values(1,'caal');
+insert into CropCategory(ID,Name) values(2,'daal');
+insert into CropCategory(ID,Name) values(3,'alu');
+insert into CropCategory(ID,Name) values(4,'piyaz');
