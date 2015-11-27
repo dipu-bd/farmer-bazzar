20bbdb0e02c2a73bbd3248596e8c47acec05a8cd
shorot <shhoroth120@gmail.com>
2015-11-27 18:37:52 +0600

    added pasword to personInfo

--- test/personInfo.sql
+++ test/personInfo.sql
@@ -1,3 +1,3 @@
-insert into personInfo (ID,ModAccess,Email,FirstName,LastName,PhoneNumber,Address,DistId) values(1,1,'dipu@gmail.com','Sudipta','Chandra','01654854554','akhaliya surma',1);
-insert into personInfo (ID,ModAccess,Email,FirstName,LastName,PhoneNumber,Address,DistId) values(2,0,'Rafi@gmail.com','Quazi','Rafi','01654854555','akhaliya surma',2);
-insert into personInfo (ID,ModAccess,Email,FirstName,LastName,PhoneNumber,Address,DistId) values(3,1,'Shorot@gmail.com','Shorot','chowdhury','01654854556','akhaliya surma',3);
+insert into personInfo (ID,ModAccess,Email,FirstName,LastName,PhoneNumber,Address,DistId) values(1,1,'dipu@gmail.com','12345','Sudipta','Chandra','01654854554','akhaliya surma',1);
+insert into personInfo (ID,ModAccess,Email,FirstName,LastName,PhoneNumber,Address,DistId) values(2,0,'Rafi@gmail.com','45678','Quazi','Rafi','01654854555','akhaliya surma',2);
+insert into personInfo (ID,ModAccess,Email,FirstName,LastName,PhoneNumber,Address,DistId) values(3,1,'Shorot@gmail.com','45678','Shorot','chowdhury','01654854556','akhaliya surma',3);
