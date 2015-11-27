e0ff6a3b39de47377c0331df371b49941c87e7b6
dipu-bd <dipu.sudipta@gmail.com>
2015-11-27 16:26:33 +0600

    database tables updated

--- Database/tables.sql
+++ Database/tables.sql
@@ -1,43 +1,61 @@
 CREATE TABLE AccessType (
 	ID INTEGER PRIMARY KEY,
-	Name VARCHAR(150),
-	Details VARCHAR(150),
-	AddProduct BOOLEAN,
-	ViewProduct BOOLEAN,
+	AddProduct BOOLEAN, /* can add product */
+	ViewProduct BOOLEAN,  /* only can view product */
+	AdminControl BOOLEAN
+);
+
+CREATE TABLE Districts (
+	ID INTEGER PRIMARY KEY, /* ID of the district */
+	Name VARCHAR(150), /* Name of the district */	
 );
 
 CREATE TABLE UserInfo (
 	ID INTEGER PRIMARY KEY,
-	TypeID INTEGER REFERENCES AccessType(ID),
-	Email VARCHAR(100) NOT NULL,
-	FirstName VARCHAR(60) NOT NULL,
-	LastName VARCHAR(60) NOT NULL,	
-	PhoneNumber LONG NOT NULL,
-	Address VARCHAR(230) NOT NULL,
+	TypeID INTEGER REFERENCES AccessType(ID), /* access type of the user */
+	Email VARCHAR(100) NOT NULL, /* email of the user */
+	FirstName VARCHAR(60) NOT NULL, /* first name */
+	LastName VARCHAR(60) NOT NULL,	/* last name */
+	PhoneNumber VARCHAR(20) NOT NULL, /* Phone number */
+	Address VARCHAR(250) NOT NULL, /* general address field for the user */
+	DistID REFERENCES Districts(ID) NOT NULL, /* id of the home district */
+	Gender VARCHAR(30),
 	DateOfBirth DATE,
 	Photo BLOB	
 );
 
+/* To store crop information */
 CREATE TABLE CropsInfo (
-	ID INTEGER PRIMARY KEY,
-	CatID INTEGER REFERENCES CropCategory(ID),
-	Name VARCHAR(150) NOT NULL,
-	Link VARCHAR(MAX)
+	ID INTEGER PRIMARY KEY, /* id of crop */
+	CatID INTEGER REFERENCES CropCategory(ID), /* leaf category id */
+	Name VARCHAR(150) NOT NULL, /* name of the crop */
 );
 
+/* Category tree of the crop */
 CREATE TABLE CropCategory (
-	ID INTEGER PRIMARY KEY,
-	Name VARCHAR(150) NOT NULL,
-	Details VARCHAR(MAX),
-	Link VARCHAR(MAX)
+	ID INTEGER PRIMARY KEY, /* id of the crop */
+	ParentID INTEGER REFERENCES CropCategory(ID), /* parent category id */
+	ChildID INTEGER REFERENCES CropCategory(ID), /* child category id */
+	Name VARCHAR(150) NOT NULL, /* name of the category */
+	Details VARCHAR(450), /* short information if necessary */
 );
 
+/* product information */
 CREATE TABLE ProductInfo (
-	ID INTEGER PRIMARY KEY,
-	Name VARCHAR(230) NOT NULL,
-	Details VARCHAR(MAX),
-	CropID INTEGER REFERENCES CropsInfo(ID),
-	Picture BLOB,
-	ManufacturingDate DATE,
-	ExpiryDate DATE,
+	ID INTEGER PRIMARY KEY, /* id of the product */
+	Name VARCHAR(230) NOT NULL,  /* name of the product */
+	Details VARCHAR(450), /* short information about the product */
+	CropID INTEGER REFERENCES CropsInfo(ID), /* id of the crop */
+	UserID INTEGER REFERENCES UserInfo(ID), /* id of the user posted */
+	Picture BLOB, /* picture of the product */
+	MfDate DATE, /* date of manufacturing */
+	ExpDate DATE, /* date of expiration */
+	PriceMin LONG, /* Minimum of the price */
+	PriceMax LONG /* Maximum of the price */	
+);
+
+CREATE TABLE RateInfo (	
+    ID INTEGER PRIMARY KEY, /* id of the rating */
+	ProdID INTEGER REFERENCES ProductInfo(ID), /* id of the product which has been rated */
+	Rating INTEGER CHECK(Rating BETWEEN 1 AND 5), /* Rating in scale of 5 */	
 );
