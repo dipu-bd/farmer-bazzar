065a39335b6ccb4f1fb8108e07371860594ac857
shorot <shhoroth120@gmail.com>
2015-11-27 20:50:20 +0600

    CropCategory  updated

--- test/cropCategory.sql
+++ test/cropCategory.sql
@@ -2,3 +2,14 @@ insert into CropCategory(ID,Name) values(1,'caal');
 insert into CropCategory(ID,Name) values(2,'daal');
 insert into CropCategory(ID,Name) values(3,'alu');
 insert into CropCategory(ID,Name) values(4,'piyaz');
+insert into CropCategory(ID,Name) values(5,'Siddho-caal');
+insert into CropCategory(ID,Name) values(6,'Atop-caal');
+insert into CropCategory(ID, Name) values(7,'Kalizira-caal');
+insert into CropCategory(ID,Name) values(8,'Deshi-piyaz');
+insert into CropCategory(ID,Name) values(9,'Indian-piyaz');
+  
+insert into CatChildren values(1,1);
+insert into CatChildren values(1,6);
+insert into CatChildren values(1,5);
+insert into CatChildren values(4,8);
+insert into CatChildren values(4,9);
