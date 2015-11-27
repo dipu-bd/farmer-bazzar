d62d3a2909b16af82a813da9c6247a1fa27eda6c
dipu-bd <dipu.sudipta@gmail.com>
2015-11-27 19:04:05 +0600

    added new table and changed table structure.

--- Database/tables.sql
+++ Database/tables.sql
@@ -14,37 +14,40 @@ CREATE TABLE PersonInfo (
 	LastName VARCHAR(60) NOT NULL,	/* last name */
 	PhoneNumber VARCHAR(20) NOT NULL, /* Phone number */	
 	Address VARCHAR(250) NOT NULL, /* general address field for the user */
-	DistID REFERENCES District(ID) NOT NULL, /* id of the home district */			
+	DistID INTEGER REFERENCES District(ID) NOT NULL, /* id of the home district */			
 	DateOfBirth DATE,	
 	Graphic BLOB
 );
 
 /* Category tree of the crop */
 CREATE TABLE CropCategory (
-	ID INTEGER PRIMARY KEY, /* id of the crop */
-	CatID INTEGER, /* id of the category */
-	ParentID INTEGER REFERENCES CropCategory(ID), /* parent category id */
-	ChildID INTEGER REFERENCES CropCategory(ID), /* child category id */
+	ID INTEGER PRIMARY KEY, /* id of the crop */	
+	ParentID INTEGER REFERENCES CropCategory(ID), /* parent category id */	
 	Name VARCHAR(150) NOT NULL, /* name of the category */
 	Details VARCHAR(450) /* short information if necessary */
+);
+
+/* child category list */
+CREATE TABLE CatChildren (
+	ParentID INTEGER REFERENCES CropCategory(ID), /* category which child it is */
+	ChildID INTEGER REFERENCES CropCategory(ID), /* ID of the child category */
 );
 
 /* To store crop information */
 CREATE TABLE CropInfo (
 	ID INTEGER PRIMARY KEY, /* id of crop */
-	CatID INTEGER, /* leaf category id */
+	CatID INTEGER REFERENCES CropInfo(ID), /* leaf category id */
 	Name VARCHAR(150) NOT NULL /* name of the crop */
 );
 
 /* product information */
 CREATE TABLE ProductInfo (
-	ID INTEGER PRIMARY KEY, /* id of the product */
-	ProdID INTEGER, /* id of the product */
+	ID INTEGER PRIMARY KEY, /* id of the product */ 
 	Name VARCHAR(230) NOT NULL,  /* name of the product */
 	Details VARCHAR(450), /* short information about the product */	
-	CropID INTEGER, /* id of the crop */
+	CropID INTEGER REFERENCES CropInfo(ID), /* id of the crop */
 	UserID INTEGER REFERENCES PersonInfo(ID), /* id of the user posted */
-	DistID REFERENCES District(ID) NOT NULL, /* id of the home district */	
+	DistID INTEGER REFERENCES District(ID) NOT NULL, /* id of the home district */	
 	MfDate DATE, /* date of manufacturing */
 	ExpDate DATE, /* date of expiration */
 	Price LONG, /* Minimum of the price */	
@@ -54,7 +57,7 @@ CREATE TABLE ProductInfo (
 /* rate of a user */
 CREATE TABLE RateInfo (	
     ID INTEGER PRIMARY KEY, /* id of the rating */
-	ProdID INTEGER, /* id of the product which has been rated */
-	UserID INTEGER,
+	ProdID INTEGER REFERENCES ProductInfo(ID), /* id of the product which has been rated */
+	UserID INTEGER REFERENCES PersonInfo(ID),
 	Rating INTEGER CHECK(Rating BETWEEN 1 AND 5) /* Rating in scale of 5 */	
 );
