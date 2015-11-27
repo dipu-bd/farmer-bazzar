fb05d9a445804de7614cd4be75bbc106781686f9
shorot <shhoroth120@gmail.com>
2015-11-27 19:06:40 +0600

    added new data

--- test/CropInfo.sql
+++ test/CropInfo.sql
@@ -1,1 +1,7 @@
-insert 
+insert into CropInfo values(1,2,'Rice');
+insert into CropInfo values(2,4,'Wheat');
+insert into CropInfo values(3,5,'Peas');
+insert into CropInfo values(4,7,'Corn');
+insert into CropInfo values(5,8,'Cauliflower');
+insert into CropInfo values(6,10,'Rice');
+insert into CropInfo values(7,11,'Rice');
